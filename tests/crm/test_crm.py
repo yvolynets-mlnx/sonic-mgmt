2f19cba4e6c747834857fc73945abd06add02588
Yuriy Volynets <yuriyv@mellanox.com>
2020-04-16 16:50:15 +0000

    Added nexthop_group test cases
    Signed-off-by: Yuriy Volynets <yuriyv@mellanox.com>

--- tests/crm/test_crm.py
+++ tests/crm/test_crm.py
@@ -22,8 +22,10 @@ THR_VERIFY_CMDS = {
 RESTORE_CMDS = {"test_crm_route": [],
                 "test_crm_nexthop": [],
                 "test_crm_neighbor": [],
+                "test_crm_nexthop_group": [],
                 "crm_cli_res": None}
 # TODO: add messages to each assert
+# TODO: create ansible wrapper for this tests
 
 
 @pytest.fixture(scope="module", autouse=True)
@@ -123,16 +125,16 @@ def test_crm_route(duthost, crm_interface, ip_ver, route_add_cmd, route_del_cmd)
 @pytest.mark.parametrize("ip_ver,nexthop", [("4", "2.2.2.2"), ("6", "2001::1")])
 def test_crm_nexthop(duthost, crm_interface, ip_ver, nexthop):
     RESTORE_CMDS["crm_cli_res"] = "ipv{ip_ver} nexthop".format(ip_ver=ip_ver)
-    nexthop_add_cmd = "ip neigh replace {nexthop} lladdr 11:22:33:44:55:66 dev {iface}"
-    nexthop_del_cmd = "ip neigh del {nexthop} lladdr 11:22:33:44:55:66 dev {iface}"
+    nexthop_add_cmd = "ip neigh replace {nexthop} lladdr 11:22:33:44:55:66 dev {iface}".format(nexthop=nexthop, iface=crm_interface[0])
+    nexthop_del_cmd = "ip neigh del {nexthop} lladdr 11:22:33:44:55:66 dev {iface}".format(nexthop=nexthop, iface=crm_interface[0])
 
     # Get "crm_stats_ipv[4/6]_nexthop" used and available counter value
     get_nexthop_stats = "redis-cli --raw -n 2 HMGET CRM:STATS crm_stats_ipv{ip_ver}_nexthop_used crm_stats_ipv{ip_ver}_nexthop_available".format(ip_ver=ip_ver)
     crm_stats_nexthop_used, crm_stats_nexthop_available = get_crm_stats(get_nexthop_stats, duthost)
 
     # Add nexthop
-    RESTORE_CMDS["test_crm_nexthop"].append(nexthop_del_cmd.format(nexthop=nexthop, iface=crm_interface[0]))
-    duthost.command(nexthop_add_cmd.format(nexthop=nexthop, iface=crm_interface[0]))
+    RESTORE_CMDS["test_crm_nexthop"].append(nexthop_del_cmd)
+    duthost.command(nexthop_add_cmd)
 
     # Make sure CRM counters updated
     time.sleep(CRM_UPDATE_TIME)
@@ -146,7 +148,7 @@ def test_crm_nexthop(duthost, crm_interface, ip_ver, nexthop):
     assert crm_stats_nexthop_available - new_crm_stats_nexthop_available >= 1
 
     # Remove nexthop
-    duthost.command(nexthop_del_cmd.format(nexthop=nexthop, iface=crm_interface[0]))
+    duthost.command(nexthop_del_cmd)
 
     # Make sure CRM counters updated
     time.sleep(CRM_UPDATE_TIME)
@@ -166,16 +168,16 @@ def test_crm_nexthop(duthost, crm_interface, ip_ver, nexthop):
 @pytest.mark.parametrize("ip_ver,neighbor", [("4", "2.2.2.2"), ("6", "2001::1")])
 def test_crm_neighbor(duthost, crm_interface, ip_ver, neighbor):
     RESTORE_CMDS["crm_cli_res"] = "ipv{ip_ver} neighbor".format(ip_ver=ip_ver)
-    neighbor_add_cmd = "ip neigh replace {neighbor} lladdr 11:22:33:44:55:66 dev {iface}"
-    neighbor_del_cmd = "ip neigh del {neighbor} lladdr 11:22:33:44:55:66 dev {iface}"
+    neighbor_add_cmd = "ip neigh replace {neighbor} lladdr 11:22:33:44:55:66 dev {iface}".format(neighbor=neighbor, iface=crm_interface[0])
+    neighbor_del_cmd = "ip neigh del {neighbor} lladdr 11:22:33:44:55:66 dev {iface}".format(neighbor=neighbor, iface=crm_interface[0])
 
     # Get "crm_stats_ipv[4/6]_neighbor" used and available counter value
     get_neighbor_stats = "redis-cli --raw -n 2 HMGET CRM:STATS crm_stats_ipv{ip_ver}_neighbor_used crm_stats_ipv{ip_ver}_neighbor_available".format(ip_ver=ip_ver)
     crm_stats_neighbor_used, crm_stats_neighbor_available = get_crm_stats(get_neighbor_stats, duthost)
 
     # Add neighbor
-    RESTORE_CMDS["test_crm_neighbor"].append(neighbor_del_cmd.format(neighbor=neighbor, iface=crm_interface[0]))
-    duthost.command(neighbor_add_cmd.format(neighbor=neighbor, iface=crm_interface[0]))
+    RESTORE_CMDS["test_crm_neighbor"].append(neighbor_del_cmd)
+    duthost.command(neighbor_add_cmd)
 
     # Make sure CRM counters updated
     time.sleep(CRM_UPDATE_TIME)
@@ -189,7 +191,7 @@ def test_crm_neighbor(duthost, crm_interface, ip_ver, neighbor):
     assert crm_stats_neighbor_available - new_crm_stats_neighbor_available >= 1
 
     # Remove neighbor
-    duthost.command(neighbor_del_cmd.format(neighbor=neighbor, iface=crm_interface[0]))
+    duthost.command(neighbor_del_cmd)
 
     # Make sure CRM counters updated
     time.sleep(CRM_UPDATE_TIME)
@@ -205,3 +207,65 @@ def test_crm_neighbor(duthost, crm_interface, ip_ver, neighbor):
     # Verify thresholds for "IPv[4/6] neighbor" CRM resource
     verify_thresholds(duthost, crm_cli_res=RESTORE_CMDS["crm_cli_res"], crm_used=new_crm_stats_neighbor_used,
         crm_avail=new_crm_stats_neighbor_available)
+
+
+@pytest.mark.parametrize("group_member,network,ip_ver", [(False, "2.2.2.0/24", "4"), (False, "2001::/126", "6"), (True, "2.2.2.0/24", "4"), (True, "2001::/126", "6")])
+def test_crm_nexthop_group(duthost, crm_interface, group_member, network, ip_ver):
+    RESTORE_CMDS["crm_cli_res"] = "nexthop group member" if group_member else "nexthop group object"
+    get_group_stats = "redis-cli --raw -n 2 HMGET CRM:STATS crm_stats_nexthop_group_used crm_stats_nexthop_group_available"
+    get_group_member_stats = "redis-cli --raw -n 2 HMGET CRM:STATS crm_stats_nexthop_group_member_used crm_stats_nexthop_group_member_available"
+    nexthop_add_cmd = "ip -{ip_ver} route add {network} nexthop via {nh_ip1} nexthop via {nh_ip2}"
+    nexthop_del_cmd = "ip -{ip_ver} route del {network} nexthop via {nh_ip1} nexthop via {nh_ip2}"
+
+    # Get "crm_stats_nexthop_group[member]" used and available counter value
+    get_nexthop_group_stats = get_group_member_stats if group_member else get_group_stats
+    nexthop_group_used, nexthop_group_available = get_crm_stats(get_nexthop_group_stats, duthost)
+
+    # Get NH IP 1
+    cmd = "ip -{ip_ver} neigh show dev {crm_intf} nud reachable nud stale".format(ip_ver=ip_ver, crm_intf=crm_interface[0])
+    out = duthost.command(cmd)
+    assert out != "", "Get Next Hop IP failed. Neighbor not found"
+    nh_ip1 = [item.split()[0] for item in out["stdout"].split("\n") if "REACHABLE" in item][0]
+
+    # Get NH IP 2
+    cmd = "ip -{ip_ver} neigh show dev {crm_intf} nud reachable nud stale".format(ip_ver=ip_ver, crm_intf=crm_interface[1])
+    out = duthost.command(cmd)
+    assert out != "", "Get Next Hop IP failed. Neighbor not found"
+    nh_ip2 = [item.split()[0] for item in out["stdout"].split("\n") if "REACHABLE" in item][0]
+
+    nexthop_add_cmd = nexthop_add_cmd.format(ip_ver=ip_ver, network=network, nh_ip1=nh_ip1, nh_ip2=nh_ip2)
+    nexthop_del_cmd = nexthop_del_cmd.format(ip_ver=ip_ver, network=network, nh_ip1=nh_ip1, nh_ip2=nh_ip2)
+
+    # Add nexthop group members
+    RESTORE_CMDS["test_crm_nexthop_group"].append(nexthop_del_cmd)
+    duthost.command(nexthop_add_cmd)
+
+    # Make sure CRM counters updated
+    time.sleep(CRM_UPDATE_TIME)
+
+    # Get new "crm_stats_nexthop_group_member" used and available counter value
+    new_nexthop_group_used, new_nexthop_group_available = get_crm_stats(get_nexthop_group_stats, duthost)
+
+    # Verify "crm_stats_nexthop_group_member_used" counter was incremented
+    assert new_nexthop_group_used - nexthop_group_used == 2
+
+    # Verify "crm_stats_nexthop_group_member_available" counter was decremented
+    assert nexthop_group_available - new_nexthop_group_available >= 2
+
+    # Remove nexthop group members
+    duthost.command(nexthop_del_cmd)
+
+    # Make sure CRM counters updated
+    time.sleep(CRM_UPDATE_TIME)
+
+    # Get new "crm_stats_nexthop_group_member" used and available counter value
+    new_nexthop_group_used, new_nexthop_group_available = get_crm_stats(get_nexthop_group_stats, duthost)
+
+    # Verify "crm_stats_nexthop_group_member_used" counter was decremented
+    assert new_nexthop_group_used - nexthop_group_used == 0
+
+    # Verify "crm_stats_nexthop_group_member_available" counter was incremented
+    assert new_nexthop_group_available - nexthop_group_available == 0
+
+    verify_thresholds(duthost, crm_cli_res=RESTORE_CMDS["crm_cli_res"], crm_used=new_nexthop_group_used,
+        crm_avail=new_nexthop_group_available)
